49a859ae63c293823cb08d9cfd95e2a3e0745092
Jaeden <149518486+JSamia36@users.noreply.github.com>
2024-12-06 08:35:01 -0800

    Create retweet.py

---
+++ linux/retweet.py
@@ -0,0 +1,52 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import Keys
+from selenium.webdriver import ActionChains
+import pickle, time, os, random
+
+print("retweeting soon")
+time.sleep((random.randint(1440,2100)))
+print("finally going to retweet")
+driver_path = 'chromedriver'  
+service = ChromeService(executable_path=driver_path)
+driver = webdriver.Chrome(service=service)
+driver.get('https://twitter.com') 
+wait = WebDriverWait(driver, 10)
+
+# ----- importing cookies -----
+with open('cookies.pkl', 'rb') as file:
+    cookies = pickle.load(file)
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+
+driver.refresh()
+time.sleep(3)
+
+driver.get('https://twitter.com/search?q=crypto&src=typed_query') 
+time.sleep(1)
+
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+time.sleep(2)
+
+like_button = driver.find_elements(By.CSS_SELECTOR, "[data-testid='retweet']")
+
+randSleep = random.randint(1,15)
+skipNum = random.choice(['on', 'off', 'on']) # if off skip tweet, if not retweet that tweet
+Num2Like = random.randint(0,3)
+
+for button in like_button[:3]:
+    if skipNum == 'on':
+        try:
+            button.click()
+            time.sleep(randSleep)
+        except:
+            continue
+    else:
+        continue
+
+print('Retweet Success')
